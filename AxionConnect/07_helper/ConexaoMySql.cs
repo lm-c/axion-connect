538fa3700a141f51b4cbc0acdb2ed080486868de
lm-c <michalakleo@gmail.com>
2025-06-16 11:29:25 -0300

    ajustes

--- AxionConnect/07_helper/ConexaoMySql.cs
+++ AxionConnect/07_helper/ConexaoMySql.cs
@@ -3,18 +3,18 @@ using System;
 
 namespace AxionConnect {
   class Prefixo {
-    public static string GetPrefixo => ""; // para Oficial         
-    // public static string GetPrefixo => "teste_"; // para testes
+    // public static string GetPrefixo => ""; // para Oficial         
+    public static string GetPrefixo => "teste_"; // para testes
   }
 
 
   public class ConexaoMySql : IDisposable {
-    public static readonly string Database = $"{Prefixo.GetPrefixo}artama";
+    public static readonly string Database = $"{Prefixo.GetPrefixo}axion";
     public static readonly string Port = "3306";
 
-    public static readonly string Server = "192.168.1.240";
-    public static readonly string User = "corbie";
-    public static readonly string Pass = "!C0rb13521!";
+    public static readonly string Server = "192.168.1.129";
+    public static readonly string User = "axion_dba";
+    public static readonly string Pass = "@@SysAxion215@@";
 
     public static MySqlConnection GetConexao() {
       return new MySqlConnection($"Server={Server};Port={Port};Database={Database};Uid={User};Pwd={Pass};SSL Mode=None");
